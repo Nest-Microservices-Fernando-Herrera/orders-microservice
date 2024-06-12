e0dcb7ab4ad868cc742d6647f0e7223086796f9e
José Barrera <josebarrerag1996@gmail.com>
2024-06-12 11:02:23 -0300

    Configurar OrdersMicroservice

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('OrdersMS-Main');
 
-  const app = await NestFactory.create(AppModule);
+  // Creando la instancia de la app e implementar el microservicio
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    // Módulo principal de entrada
+    AppModule,
+    {
+      /* Opciones de configuración */
+      transport: Transport.TCP, // Medio de transporte,
+      options: {
+        port: envs.port
+      }
+    }
+  );
 
-  await app.listen(envs.port);
+  // Configuración global de los Pipes
+  app.useGlobalPipes(new ValidationPipe({
+    // Habilitandod los DTOs
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
+
+  await app.listen();
   logger.log(`Microservice running on port ${envs.port}`);
 }
 bootstrap();
